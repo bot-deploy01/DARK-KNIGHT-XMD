bd99c0dad6c9e124ce8a711d37649e7177d11ddf
bot-deploy-main <152987682+bot-deploy-main@users.noreply.github.com>
2026-04-21 20:26:25 +0530

    Update main-env.js

--- plugins/main-env.js
+++ plugins/main-env.js
@@ -1,4 +1,160 @@
-const config = require('../config');
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const os = require("os")
+
+cmd({
+    pattern: "settings",
+    alias: ["setting"],
+    desc: "settings the bot",
+    category: "owner",
+    react: "⚙",
+    filename: __filename
+
+
+},
+async (conn, mek, m, { from, isOwner, quoted, reply }) => {
+    if (!isOwner) return reply("❌ You are not the owner!");
+    try {
+        let desc = `* _𝑺𝑬𝑻𝑻𝑰𝑵𝑮𝑺_
+
+
+╭══════════════════════○
+┣━ *𝗪𝗢𝗥𝗞 𝗠𝗢𝗗𝗘 😈*
+> *1️⃣.1️⃣  Public Work*
+> *1️⃣.2️⃣  Private Work*
+> *1️⃣.3️⃣  Group Only*
+> *1️⃣.4️⃣  Inbox Only*
+╭══════════════════════○
+┣━ *𝗔𝗨𝗧𝗢 𝗩𝗢𝗜𝗖𝗘 😈*
+> *2️⃣.1️⃣ Auto Voice On*
+> *2️⃣.2️⃣ Auto Voice Off*
+╭══════════════════════○
+┣━ *𝗔𝗨𝗧𝗢 𝗦𝗧𝗔𝗧𝗨𝗦 𝗦𝗘𝗘𝗡 😈*
+> *3️⃣.1️⃣ Auto Read Status On*
+> *3️⃣.2️⃣ Auto Read Status Off*
+╭══════════════════════○
+┣━ *𝗔𝗨𝗧𝗢 𝗦𝗧𝗜𝗖𝗞𝗘𝗥 😈*
+> *4️⃣.1️⃣ Auto sticker On*
+> *4️⃣.2️⃣ Auto sticker Off*
+╭══════════════════════○
+┣━ *𝗔𝗨𝗧𝗢 𝗥𝗘𝗣𝗟𝗬😈*
+> *5️⃣.1️⃣ Auto reply On*
+> *5️⃣.2️⃣ Auto reply Off*
+╭══════════════════════○
+┣━ *𝗕𝗢𝗧 𝗢𝗡𝗟𝗜𝗡𝗘 𝗢𝗙𝗙𝗟𝗜𝗡𝗘 😈*
+> *6️⃣.1️⃣ Online On*
+> *6️⃣.2️⃣ Online Off*
+╭══════════════════════○
+┣━ *𝗠𝗦𝗚 𝗥𝗘𝗔𝗗 😈*
+> *7️⃣.1️⃣ Read Msg On*
+> *7️⃣.2️⃣ Read Msg Off*
+╭══════════════════════○
+┣━ *𝗠𝗦𝗚 𝗥𝗘𝗔𝗖𝗧 😈*
+> *8️⃣.1️⃣ Auto React On*
+> *8️⃣.2️⃣ Auto React Off*
+╭══════════════════════○
+┣━ *𝗔𝗡𝗧𝗜 𝗟𝗜𝗡𝗞 😈*
+> *9️⃣.1️⃣ Anti Link On*
+> *9️⃣.2️⃣ Anti Link Off*
+> *9️⃣.3️⃣ Anti Link Remove*
+╰═══════════════════════○
+
+
+* *🔢 Reply Below This Number Change To Bot Change Setting*
+
+> powerd by 𝙳𝙰𝚁𝙺-𝙺𝙽𝙸𝙶𝙷𝚃-𝚇𝙼𝙳`;
+
+        const vv = await conn.sendMessage(from, { image: { url: config.MENU_IMAGE_URL }, caption: desc }, { quoted: mek });
+
+        conn.ev.on('messages.upsert', async (msgUpdate) => {
+            const msg = msgUpdate.messages[0];
+            if (!msg.message || !msg.message.extendedTextMessage) return;
+
+            const selectedOption = msg.message.extendedTextMessage.text.trim();
+
+            if (msg.message.extendedTextMessage.contextInfo && msg.message.extendedTextMessage.contextInfo.stanzaId === vv.key.id) {
+                switch (selectedOption) {
+                    case '1.1':
+                        reply(".update MODE:public" );
+                        break;
+                    case '1.2':               
+                        reply(".update MODE:private");
+                        break;
+                    case '1.3':               
+                          reply(".update MODE:group");
+                      break;
+                    case '1.4':     
+                        reply(".update MODE:inbox");
+                      break;
+                    case '2.1':     
+                        reply(".update AUTO_VOICE:true");
+                        break;
+                    case '2.2':     
+                        reply(".update AUTO_VOICE:false");
+                    break;
+                    case '3.1':    
+                        reply(".update AUTO_READ_STATUS:true");
+                    break;
+                    case '3.2':    
+                        reply(".update AUTO_READ_STATUS:false");
+                    break;                    
+                    case '4.1':    
+                        reply(".update AUTO_STICKER:true");
+                    break;
+                    case '4.2':    
+                        reply(".update AUTO_STICKER:false");
+                    break;                                        
+                    case '5.1':    
+                        reply(".update AUTO_REPLY:true");
+                    break;
+                    case '5.2':    
+                        reply(".update AUTO_REPLY:false");
+                    break;                        
+                    case '6.1':    
+                        reply(".update ALLWAYS_OFFLINE:true");
+                    break; 
+                    case '6.2':    
+                        reply(".update ALLWAYS_OFFLINE:false");
+                    break;                       
+                    case '7.1':    
+                        reply(".update READ_MESSAGE:true");
+                    break;
+                    case '7.2':    
+                        reply(".update READ_MESSAGE:false");
+                    break;
+                    case '8.1':    
+                        reply(".update config.AUTO_REACT:true");
+                    break;
+                    case '8.2':    
+                        reply(".update config.AUTO_REACT:false");
+                    break;
+                    case '9.1':    
+                        reply(".update ANTI_LINK:true");
+                        reply(".update ANTI_LINKK:false");
+                    break;
+                    case '9.2':    
+                        reply(".update ANTI_LINKK:true");
+                        reply(".update ANTI_LINK:false");
+                    break;
+                    case '9.3':    
+                        reply(".update ANTI_LINK:false");
+                        reply(".update ANTI_LINKK:false");
+                    break;
+                    default:
+                        reply("Invalid option. Please select a valid option🔴");
+                }
+
+            }
+        });
+
+    } catch (e) {
+        console.error(e);
+        await conn.sendMessage(from, { react: { text: '❌', key: mek.key } })
+        reply('An error occurred while processing your request.');
+    }
+});
+
+/*const config = require('../config');
 const { cmd, commands } = require('../command');
 const { runtime } = require('../lib/functions');
 const axios = require('axios');
@@ -167,4 +323,4 @@ async (conn, mek, m, { from, quoted, reply, isCreator }) => {
         console.error('Env command error:', error);
         reply(`❌ Error displaying config: ${error.message}`);
     }
-});
+});*/

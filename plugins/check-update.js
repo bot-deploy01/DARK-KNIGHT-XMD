300514758dad086f66f2db1a5a324a3c05f8edb6
bot-deploy-main <152987682+bot-deploy-main@users.noreply.github.com>
2026-05-28 07:09:18 +0530

    Update check-update.js

--- plugins/check-update.js
+++ plugins/check-update.js
@@ -87,7 +87,7 @@ Use *.update* to update.`;
     
     // Send the status message with an image
     await conn.sendMessage(from, {
-      image: { url: 'https://files.catbox.moe/brlkte.jpg' },
+      image: { url: config.ALIVE_IMG },
       caption: statusMessage,
       contextInfo: {
         mentionedJid: [m.sender],
